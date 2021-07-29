fe0c328807064df5dd55899cf9e58552b3cf393e
Francis Barcebal <francis.barcebal@amadeus.com>
2021-07-29 15:15:47 +0800

    Added first component (No module)

---
+++ ng-movie/src/app/models/movie.ts
@@ -0,0 +1,19 @@
+export interface Movie {
+  id: number;
+  title: string;
+  phase: string;
+  categoryName?: string;
+  releaseYear: number;
+  runningTime: number;
+  ratingName: string;
+  discFormatName: string;
+  numberDiscs: number;
+  viewingFormatName: string;
+  aspectRatioName: string;
+  status: number;
+  releaseDate: string;
+  budget: string;
+  gross: string;
+  timeStamp: string;
+  edition?: string;
+}
