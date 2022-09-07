f58e2a5e79f1b8e8fd20cb01255f4a743255e287
Kaye CALUNGSOD <kaye.calungsod@amadeus.com>
2022-09-07 08:08:31 +0800

    refactored impure pipe - filterByPriorityTag

--- todo-app/src/app/pipes/filterByPriorityTag.pipe.ts
+++ todo-app/src/app/pipes/filterByPriorityTag.pipe.ts
@@ -10,12 +10,7 @@ export class FilterByPriorityTagPipe implements PipeTransform {
   transform(taskEntries: TaskEntry[], priorityTags: string[]): any {
     if (priorityTags.length < 1) { return taskEntries; }
     return taskEntries.filter((taskEntry) => {
-      for (let x = 0; x < priorityTags.length; x++) {
-        if (taskEntry.priorityTag === priorityTags[x]) {
-          return true;
-        }
-      }
-      return false;
+      return !taskEntry.priorityTag ? false : (priorityTags.includes(taskEntry.priorityTag) ? true : false);
     })
   }
 }
